6e3b291e29d9cdeee9b6ab1a6dfa17068b7b8244
applewebbo <83427957+applewebbo@users.noreply.github.com>
2023-06-07 08:43:54 +0200

    added django-allauth and configuration

--- core/settings.py
+++ core/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     ALLOWED_HOSTS=(list, []),
     DEBUG=(bool, False),
+    ACCOUNT_DEFAULT_HTTP_PROTOCOL=(str, "https"),
 )
 environ.Env.read_env(BASE_DIR / ".env")
 
@@ -38,7 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.sites",
     "django.contrib.staticfiles",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
     "better_todo.accounts",
 ]
 
@@ -114,13 +119,18 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "accounts.User"
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "it-it"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Rome"
 
 USE_I18N = True
 
@@ -130,12 +140,63 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# SITES
+
+SITE_ID = 1
+
+# DJANGO-ALLAUTH
+
+# ACCOUNT_ADAPTER => default
+# ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS => default
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+# TODO: set LOGIN_URL
+# ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL => default
+# TODO: set LOGIN_REDIRECT_URL
+# ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL => default
+# ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS => default
+# ACCOUNT_EMAIL_CONFIRMATION_HMAC => default
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_SUBJECT_PREFIX = "BetterTodo - "
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = env("ACCOUNT_DEFAULT_HTTP_PROTOCOL")
+# ACCOUNT_EMAIL_CONFIRMATION_COOLDOWN => default
+# ACCOUNT_EMAIL_MAX_LENGTH => default
+# ACCOUNT_MAX_EMAIL_ADDRESSES => default
+# ACCOUNT_FORMS => default
+# ACCOUNT_LOGIN_ATTEMPTS_LIMIT => default
+# ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT => default
+ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
+# ACCOUNT_LOGOUT_ON_GET => default
+# ACCOUNT_LOGOUT_ON_PASSWORD_CHANGE => default
+# ACCOUNT_LOGIN_ON_PASSWORD_RESET => default
+# ACCOUNT_LOGOUT_REDIRECT_URL => default
+# ACCOUNT_PASSWORD_INPUT_RENDER_VALUE => default
+ACCOUNT_PRESERVE_USERNAME_CASING = False
+# ACCOUNT_PREVENT_ENUMERATION => default
+# ACCOUNT_RATE_LIMITS => default
+ACCOUNT_SESSION_REMEMBER = True
+# ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE => default
+# ACCOUNT_SIGNUP_FORM_CLASS => default
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+# ACCOUNT_SIGNUP_REDIRECT_URL => default
+# ACCOUNT_TEMPLATE_EXTENSION => default
+# ACCOUNT_USERNAME_BLACKLIST => default
+# ACCOUNT_UNIQUE_EMAIL => default
+ACCOUNT_USER_DISPLAY = lambda user: user.email  # noqa
+# ACCOUNT_USER_MODEL_EMAIL_FIELD => default
+# ACCOUNT_USER_MODEL_USERNAME_FIELD => default
+# ACCOUNT_USERNAME_MIN_LENGTH => default
+ACCOUNT_USERNAME_REQUIRED = False
+# ACCOUNT_USERNAME_VALIDATORS => default
+# SOCIALACCOUNT_* => default
